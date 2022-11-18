dc95746b49289cc563ff535e84ca88a4e3ceda7d
LuciferAmani <reza_amani@icloud.com>
2022-11-18 10:42:41 +0100

    smells bettter now

--- test/cars/Saab95Test.java
+++ test/cars/Saab95Test.java
@@ -4,16 +4,28 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class Saab95Test {
-  Saab95 testSaab = new Saab95();
+  
+
   @Test 
   public void see_if_speedfactor_is_correctly_calculated() {
+    Saab95 testSaab = new Saab95();
     assertTrue(testSaab.speedFactor() == testSaab.getEnginePower()*0.01);
   }
+  
   @Test
   public void see_if_turbo_method_change_speedfactor_according_to_plans() {
+    Saab95 testSaab = new Saab95();
     assertTrue(testSaab.speedFactor() == testSaab.getEnginePower()*0.01);
+  }
+  public void see_if_turbo_is_on(){
+    Saab95 testSaab = new Saab95();
     testSaab.setTurboOn();
     assertTrue(testSaab.speedFactor() == testSaab.getEnginePower()*0.01*1.3);
+  }
+
+  @Test
+    public void see_if_turbo_is_off(){
+      Saab95 testSaab = new Saab95();
     testSaab.setTurboOff();
     assertTrue(testSaab.speedFactor() == testSaab.getEnginePower()*0.01);
     }
