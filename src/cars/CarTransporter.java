e09ae029a14dcd2cc5256563d83bb0ecbb0f8c12
Reza Amani <113048496+RezaAmanii@users.noreply.github.com>
2022-11-25 13:18:10 +0100

    Added more options

--- src/cars/CarTransporter.java
+++ src/cars/CarTransporter.java
@@ -3,16 +3,30 @@
 public class CarTransporter extends Truck{
 
     private Platform platform;
-    privat boolean isLoaded;
+    private boolean isLoaded;
+    private int storageLimitation = 10;
 
     CarTransporter(int nrDoors, double enginePower, Color color, String modelName, boolean isLoaded){
         this.platform = new Platform();
         this.isLoaded = isLoaded;
         super(nrDoors, enginePower, color, modelName);
-        
+    }
+
 
     public double speedFactor() {
         return getEnginePower() * 0.005;
     }
+
+    // Making a list of cars which will or are loaded
+    Car[] storageList = new Car(10);
+
+    // 
+    public boolean loadON(){
+        if(storageList < 10 && isLoaded == false){
+            return true;
+        } else {
+            return false;
+
+        } 
     }
-}
+    }
