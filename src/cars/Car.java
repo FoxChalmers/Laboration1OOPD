4518cdc674e6bb9d955aae76dd3b2158c05a6740
FoxChalmers <114472006+FoxChalmers@users.noreply.github.com>
2022-12-06 15:28:04 +0100

    Fixed errors

--- src/cars/Car.java
+++ src/cars/Car.java
@@ -1,8 +1,10 @@
 package cars;
 
-interface Car {
+public interface Car {
     int getX();
     int getY();
     void setX(int x);
     void setY(int y);
+    void move();
+    void gas(double amount);
 }
