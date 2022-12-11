f35f25e7aa4ee7a3b36c7bc6c2648086c92f28ac
FoxChalmers <114472006+FoxChalmers@users.noreply.github.com>
2022-12-11 03:17:11 +0100

    Update Observable.java

--- src/application/Observable.java
+++ src/application/Observable.java
@@ -1,9 +1,11 @@
 package application;
 
 public interface Observable {
+
     public void updateObservers();
     public void addObserver(Observer o);
     public void removeObserver(Observer o);
+    
 }
 
 // Observable interface used for classes that want to be observed by observers.
